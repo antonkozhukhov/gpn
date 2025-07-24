89d5fa1e8a46648124e7cba2ce91581fb7d03d32
Anton Kozhukhov <antonkozhukhov@yandex.ru>
2025-07-24 17:46:09 +0700

    Initial GPN calculator2

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.getElementById('gpnForm').addEventListener('submit', async function(e) 
     
     try {
         // Для локального тестирования с Flask-сервером
-        const response = await fetch('https://shaggy-places-repeat.loca.lt/api/calculate', {
+        const response = await fetch('https://shaggy-places-repeat.loca.lt/', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ pressure, depth })
