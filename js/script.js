bdbd102777f7e9000769f59666b86c519dea61b5
kozhukhov <antonkozhukhov@yandex.ru>
2025-10-15 11:03:42 +0700

    002

--- js/script.js
+++ js/script.js
@@ -5,11 +5,11 @@ let API_BASE_URL;
 try {
   const config = await import('./config.js');
   //API_BASE_URL = config.API_BASE_URL;
-  API_BASE_URL = 'https://pay-burns-minute-hanging.trycloudflare.com';
+  API_BASE_URL = 'https://0dec8a0f7e13ce8bd9e60f9cccc879ee.serveo.net';
   console.log(API_BASE_URL)
 
 } catch {
-  API_BASE_URL = 'https://pay-burns-minute-hanging.trycloudflare.com';
+  API_BASE_URL = 'https://0dec8a0f7e13ce8bd9e60f9cccc879ee.serveo.net';
 }
 
 // Вспомогательная функция для безопасного получения значений
